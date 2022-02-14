daf0387a20b101e5034d740f7f062b50186f5844
Emeric Francisod <emeric.francisod+github@gmail.com>
2022-02-14 17:30:53 +0100

    Add keyboard action to reject focus: escape

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
+// Manage scroll to top
 $(".scrollToTop").click(function(e) {
     e.preventDefault();
     scrollToTarget('1');
     document.activeElement.blur();
+});
+
+//Focus rejection on Escape
+$(document).keyup(function(e) {
+    if (e.key === "Escape") {
+        document.activeElement.blur();
+    }
 });
